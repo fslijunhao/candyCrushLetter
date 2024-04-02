4995b8dd1b4cf50e69e6079c8f2a6a2e20a7422d
ljh <287540659@qq.com>
2024-04-02 12:48:30 +0800

    init

---
+++ src/test/java/test/MyTestClass.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+public class MyTestClass {
+    @Test
+    public void myTestMethod() {
+    // 编写测试逻辑
+        System.out.println("sdf");
+    }
+}
