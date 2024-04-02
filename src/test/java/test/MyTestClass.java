f2b276321f8303037a2ef37e970cc40b4e62f8f8
ljh <287540659@qq.com>
2024-04-02 16:23:05 +0800

    submit

--- src/test/java/test/MyTestClass.java
+++ src/test/java/test/MyTestClass.java
@@ -2,10 +2,70 @@ package test;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.StringTokenizer;
+
 public class MyTestClass {
+
+
     @Test
-    public void myTestMethod() {
-    // 编写测试逻辑
-        System.out.println("sdf");
+    public void test1() {
+        String input = "aabcccbbad";
+        String consecutiveStr;
+        while ((consecutiveStr = this.findConsecutive(input)) != null) {
+            input = input.replace(consecutiveStr, "");
+            System.out.println("-> " + input);
+        }
+    }
+
+    @Test
+    public void test2() {
+        String input = "abcccbad";
+        String consecutiveStr;
+        while ((consecutiveStr = this.findConsecutive(input)) != null) {
+            //find the char before it
+            char c = consecutiveStr.charAt(0);
+            c = (char) (c - 1);
+            if (c >= 'a') { // ensure not less than 'a'
+                input = input.replace(consecutiveStr, String.valueOf(c));
+                System.out.println("-> " + input + ", " + consecutiveStr + " is replaced by " + c);
+            } else {
+                input = input.replace(consecutiveStr, "");
+                System.out.println("-> " + input);
+            }
+
+        }
+
+//        System.out.println(  (char)('b'-1) );
+//        System.out.println(  String.valueOf('b').getClass() );
+//        System.out.println('c' > 'a');
+    }
+
+
+    private String findConsecutive(String str) {
+        String prev = null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            String s = str.substring(i, i + 1);
+            if (prev == null || s.equals(prev)) {
+//                System.out.println(sb + ", ");
+            } else { // if not consecutive
+
+                String output = sb.toString();
+                if (output.length() >= 3) { // if found more than 3 chars
+                    return output;
+                }
+                sb.setLength(0);
+            }
+
+            prev = s;
+            sb.append(s);
+        }
+
+        String output = sb.toString();
+        if (output.length() >= 3) { // if found more than 3 chars
+            return output;
+        } else {
+            return null;
+        }
     }
 }
